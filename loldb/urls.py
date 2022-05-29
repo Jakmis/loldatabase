7b1b94ec64f30a6b8346d97dea44ef80604a3d7e
jakmis <jakmis007gmail.com>
2022-05-29 22:47:49 +0200

    verze 1.4

--- loldb/urls.py
+++ loldb/urls.py
@@ -2,5 +2,7 @@ from django.urls import  path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('champions/', views.ChampionListView.as_view(), name='champions'),
+    path('champions/<int:pk>', views.ChampionDetailView.as_view(), name='champions_detail'),
 ]
--- loldb/views.py
+++ loldb/views.py
@@ -1,7 +1,34 @@
-import re
+
+from django.views.generic import ListView, DetailView
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from loldb.models import Champion
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+
+    num_champions = Champion.objects.all().count()
+
+    champions = Champion.objects.order_by('name')[:3]
+
+    context = {
+            'num_champions': num_champions,
+            'champions': champions,
+    }
+
+    return render(request, 'index.html', context=context)
+
+class ChampionListView(ListView):
+    model = Champion
+
+    context_object_name = 'champions'
+
+    template_name = 'list.html'
+
+class ChampionDetailView(DetailView):
+    model = Champion
+
+    context_object_name = 'champions_detail'
+
+    template_name = 'detail.html'
