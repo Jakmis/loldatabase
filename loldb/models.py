fb40c3e9a032f952c99e1385df06b19dc8571241
jakmis <jakmis007gmail.com>
2022-06-05 23:15:59 +0200

    verze 1.6

--- loldb/models.py
+++ loldb/models.py
@@ -1,4 +1,3 @@
-from distutils.command.upload import upload
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
--- loldb/views.py
+++ loldb/views.py
@@ -1,9 +1,8 @@
 
 from django.views.generic import ListView, DetailView
-from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
-from loldb.models import Champion
+from loldb.models import Champion, Region
 # Create your views here.
 
 def index(request):
@@ -12,9 +11,12 @@ def index(request):
 
     champions = Champion.objects.order_by('name')[:3]
 
+    regions = Region.objects.all()
+
     context = {
             'num_champions': num_champions,
             'champions': champions,
+            'regions':regions,
     }
 
     return render(request, 'index.html', context=context)
